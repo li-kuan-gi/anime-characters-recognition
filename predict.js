760d20098757c935baefeef8db5537ddd5f746bb
li-kuan-gi <82380617+li-kuan-gi@users.noreply.github.com>
2022-06-22 05:58:52 +0000

    improve UI

---
+++ predict.js
@@ -0,0 +1,77 @@
+const sessionPromise = ort.InferenceSession.create('./model/anime.onnx');
+
+const imageInput = document.getElementById("image-input");
+imageInput.addEventListener("change", handleImageInput, false);
+
+function handleImageInput() {
+    const canvas = document.querySelector("canvas");
+    const [image, url] = getImageAndUrl(this.files[0])
+
+    image.onload = () => {
+        clearResult();
+        draw(image, canvas, 256);
+        const imageData = getImageData(canvas);
+        window.URL.revokeObjectURL(url);
+        sessionPromise.then((session) => { predictCharacter(imageData, session); })
+    }
+}
+
+async function predictCharacter(imageData, session) {
+    data = new Float32Array(extract(imageData.data));
+
+    const input = new ort.Tensor("float32", data, [1, 3, 224, 224]);
+
+    const outputMap = await session.run({ 'input': input });
+    const prediction = outputMap.output.data;
+    const maxPrediction = prediction.indexOf(Math.max(...prediction));
+
+
+    showResult(window.characters[maxPrediction]);
+}
+
+function extract(data) {
+    const redArray = data.filter(function (v, i, a) { return i % 4 == 0; })
+    const greenArray = data.filter(function (v, i, a) { return i % 4 == 1; })
+    const blueArray = data.filter(function (v, i, a) { return i % 4 == 2; })
+    return [...redArray, ...greenArray, ...blueArray].map(function (i) { return i / 255; });
+}
+
+function getImageAndUrl(file) {
+    const image = new Image()
+    const url = window.URL.createObjectURL(file)
+    image.src = url
+    return [image, url]
+}
+
+function draw(image, canvas, resize) {
+    const ctx = canvas.getContext('2d');
+    let max_side = Math.max(image.width, image.height);
+    let min_side = Math.min(image.width, image.height);
+    max_side *= resize / min_side;
+    min_side = resize;
+    target_size = image.width > image.height ? [max_side, min_side] : [min_side, max_side]
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    canvas.width = target_size[0];
+    canvas.height = target_size[1];
+    ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, target_size[0], target_size[1]);
+}
+
+function getImageData(canvas) {
+    const ctx = canvas.getContext('2d');
+
+    const imageData = ctx.getImageData((canvas.width - 224) / 2, (canvas.width - 224) / 2, 224, 224)
+    return imageData;
+}
+
+function showResult(character) {
+    const display = document.getElementById("prediction");
+    display.replaceChildren();
+    display.innerHTML = `${character.work},  ${character.name}`;
+}
+
+function clearResult() {
+    const display = document.getElementById("prediction");
+    const loader = document.createElement("div");
+    loader.className = "loader";
+    display.appendChild(loader);
+}
--- script.js
+++ script.js
@@ -1,79 +1,23 @@
-const sessionPromise = ort.InferenceSession.create('./model/anime.onnx');
 fetch("./model/characters.json")
     .then((r) => r.json())
-    .then((json) => characters = json);
+    .then((json) => {
+        window.characters = json;
+        json.forEach(char => createCharacterInfo(char));
+    });
 
-window.onload = function () {
-    const imageInput = document.getElementById("image-input");
-    imageInput.addEventListener("change", handleImageInput, false);
-}
+function createCharacterInfo(char) {
+    const table = document.getElementById('support');
 
-function handleImageInput() {
-    const canvas = document.querySelector("canvas");
-    const [image, url] = getImageAndUrl(this.files[0])
+    const row = document.createElement('tr');
 
-    image.onload = () => {
-        clearResult();
-        draw(image, canvas, 256);
-        const imageData = getImageData(canvas);
-        window.URL.revokeObjectURL(url);
-        sessionPromise.then((session) => { predictCharacter(imageData, session); })
-    }
-}
+    const work = document.createElement('td');
+    work.innerHTML = char.work;
 
-async function predictCharacter(imageData, session) {
-    data = new Float32Array(extract(imageData.data));
+    const name = document.createElement('td');
+    name.innerHTML = char.name;
 
-    const input = new ort.Tensor("float32", data, [1, 3, 224, 224]);
+    row.appendChild(work);
+    row.appendChild(name);
 
-    const outputMap = await session.run({ 'input': input });
-    const prediction = outputMap.output.data;
-    const maxPrediction = prediction.indexOf(Math.max(...prediction));
-
-
-    showResult(characters[maxPrediction]);
-}
-
-function extract(data) {
-    const redArray = data.filter(function (v, i, a) { return i % 4 == 0; })
-    const greenArray = data.filter(function (v, i, a) { return i % 4 == 1; })
-    const blueArray = data.filter(function (v, i, a) { return i % 4 == 2; })
-    return [...redArray, ...greenArray, ...blueArray].map(function (i) { return i / 255; });
-}
-
-function getImageAndUrl(file) {
-    const image = new Image()
-    const url = window.URL.createObjectURL(file)
-    image.src = url
-    return [image, url]
-}
-
-function draw(image, canvas, resize) {
-    const ctx = canvas.getContext('2d');
-    let max_side = Math.max(image.width, image.height);
-    let min_side = Math.min(image.width, image.height);
-    max_side *= resize / min_side;
-    min_side = resize;
-    target_size = image.width > image.height ? [max_side, min_side] : [min_side, max_side]
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    canvas.width = target_size[0];
-    canvas.height = target_size[1];
-    ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, target_size[0], target_size[1]);
-}
-
-function getImageData(canvas) {
-    const ctx = canvas.getContext('2d');
-
-    const imageData = ctx.getImageData((canvas.width - 224) / 2, (canvas.width - 224) / 2, 224, 224)
-    return imageData;
-}
-
-function showResult(character) {
-    const display = document.getElementById("prediction");
-    display.innerHTML = `${character.work},  ${character.name}`;
-}
-
-function clearResult() {
-    const display = document.getElementById("prediction");
-    display.innerHTML = '';
+    table.appendChild(row);
 }
